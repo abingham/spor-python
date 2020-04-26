91fb21cdb551c0ca86a41cac5656e724cae31442
Austin Bingham <austin.bingham@gmail.com>
2020-04-26 11:38:26 +0200

    Progress on pyo3 version.

--- src/anchor.rs
+++ src/anchor.rs
@@ -1,113 +1,160 @@
-use cpython::ObjectProtocol;
-use cpython::{py_class, PyDict, PyErr, PyModule, PyResult, Python};
+use pyo3::prelude::*;
 
 // TODO: Can we properly model the ownership relationship between Anchor and Context? Right now we just copy contexts
 // when we need them from the anchor.
 
-py_class!(pub class Context |py| {
-    def __new__(_cls, text: &str, offset: usize, width: usize, context_width: usize) -> PyResult<Context> {
-        spor::anchor::Context::new(text, offset, width, context_width)
-            .or_else(|err| {
-                Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err)))
-            })
-            .and_then(|context| {
-                Context::create_instance(py, context)
-            })
-    }
-
-    def before(&self) -> PyResult<String> {
-        Ok(self.context(py).before().clone())
-    }
-
-    def offset(&self) -> PyResult<usize> {
-        Ok(self.context(py).offset())
-    }
-
-    def topic(&self) -> PyResult<String> {
-        Ok(self.context(py).topic().clone())
-    }
-
-    def after(&self) -> PyResult<String> {
-        Ok(self.context(py).after().clone())
-    }
-
-    def width(&self) -> PyResult<usize> {
-        Ok(self.context(py).width())
-    }
-
-    def full_text(&self) -> PyResult<String> {
-        Ok(self.context(py).full_text())
-    }
-
-    data context: spor::anchor::Context;
-});
-
-py_class!(pub class Anchor |py| {
-    def __new__(_cls,
-        file_path: String,
-        context: Context,
-        metadata: PyDict,
-        encoding: String
-    ) -> PyResult<Anchor> {
-        let file_path = std::path::Path::new(&file_path);
-        let metadata = PyModule::import(py, "yaml")
-            .and_then(|yaml| yaml.get(py, "dump"))
-            .and_then(|dump| dump.call(py, (metadata,), None))
-            .and_then(|string| string.extract::<String>(py))
-            .and_then(|string|
-                serde_yaml::from_str::<serde_yaml::Value>(&string)
-                    .or_else(|err|
-                        Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err)))))?;
-
-        let context = context.context(py).clone();
-
-        spor::anchor::Anchor::new(file_path, context, metadata, encoding)
-            .or_else(|err| {
-                Err(PyErr::new::<cpython::exc::OSError, _>(py, format!("{}", err)))
-            })
-            .and_then(|anchor| {
-                Anchor::create_instance(py, anchor)
-            })
-    }
-
-    def file_path(&self) -> PyResult<String> {
-        let p = self.anchor(py).file_path();
-        p.to_str()
-            .ok_or(PyErr::new::<cpython::exc::ValueError, _>(py,
-                "Unable to convert path to string"))
-            .map(|s| s.to_owned())
-    }
-
-    def encoding(&self) -> PyResult<String> {
-        Ok(self.anchor(py).encoding().clone())
-    }
-
-    def context(&self) -> PyResult<Context> {
-        Context::create_instance(py, self.anchor(py).context().clone())
-    }
-
-    def metadata(&self) -> PyResult<PyDict> {
-        let metadata = self.anchor(py).metadata();
-
-        let metadata_string = serde_yaml::to_string(metadata)
-            .or_else(|err|
-                Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err))))?;
-
-        PyModule::import(py, "yaml")
-            .and_then(|yaml| yaml.get(py, "safe_load"))
-            .and_then(|load| load.call(py, (metadata_string,), None))
-            .and_then(|dict|
-                dict.cast_into::<PyDict>(py)
-                    .or_else(|err| Err(PyErr::from(err))))
-    }
-
-    data anchor: spor::anchor::Anchor;
-});
-
-pub fn init_module(py: Python) -> PyResult<PyModule> {
-    let m = PyModule::new(py, "anchor")?;
-    m.add(py, "__doc__", "Anchor implementation")?;
-    m.add_class::<Anchor>(py)?;
-    m.add_class::<Context>(py)?;
-    Ok(m)
+#[pyclass(name=Context, module="spor.anchor")]
+pub struct PyContext {
+    handle: spor::anchor::Context
 }
+
+#[pymethods]
+impl PyContext {
+    #[new]
+    fn new(text: &str, offset: usize, width: usize, context_width: usize) -> PyResult<Self> {
+        spor::anchor::Context::new(text, offset, width, context_width) 
+            .or_else(|err| 
+                Err(pyo3::exceptions::ValueError::py_err(format!("{}", err))))
+            .map(|context| PyContext { handle: context})
+    }
+
+    #[getter]
+    fn before(&self) -> PyResult<String> {
+        Ok(self.handle.before().clone())
+    }
+
+    #[getter]
+    fn offset(&self) -> PyResult<usize> {
+        Ok(self.handle.offset())
+    }
+
+    #[getter]
+    fn topic(&self) -> PyResult<String> {
+        Ok(self.handle.topic().clone())
+    }
+
+    #[getter]
+    fn after(&self) -> PyResult<String> {
+        Ok(self.handle.after().clone())
+    }
+
+    #[getter]
+    fn width(&self) -> PyResult<usize> {
+        Ok(self.handle.width())
+    }
+
+    #[getter]
+    fn full_text(&self) -> PyResult<String> {
+        Ok(self.handle.full_text())
+    }
+}
+
+
+
+// py_class!(pub class Context |py| {
+//     def __new__(_cls, text: &str, offset: usize, width: usize, context_width: usize) -> PyResult<Context> {
+//         spor::anchor::Context::new(text, offset, width, context_width)
+//             .or_else(|err| {
+//                 Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err)))
+//             })
+//             .and_then(|context| {
+//                 Context::create_instance(py, context)
+//             })
+//     }
+
+//     def before(&self) -> PyResult<String> {
+//         Ok(self.context(py).before().clone())
+//     }
+
+//     def offset(&self) -> PyResult<usize> {
+//         Ok(self.context(py).offset())
+//     }
+
+//     def topic(&self) -> PyResult<String> {
+//         Ok(self.context(py).topic().clone())
+//     }
+
+//     def after(&self) -> PyResult<String> {
+//         Ok(self.context(py).after().clone())
+//     }
+
+//     def width(&self) -> PyResult<usize> {
+//         Ok(self.context(py).width())
+//     }
+
+//     def full_text(&self) -> PyResult<String> {
+//         Ok(self.context(py).full_text())
+//     }
+
+//     data context: spor::anchor::Context;
+// });
+
+// py_class!(pub class Anchor |py| {
+//     def __new__(_cls,
+//         file_path: String,
+//         context: Context,
+//         metadata: PyDict,
+//         encoding: String
+//     ) -> PyResult<Anchor> {
+//         let file_path = std::path::Path::new(&file_path);
+//         let metadata = PyModule::import(py, "yaml")
+//             .and_then(|yaml| yaml.get(py, "dump"))
+//             .and_then(|dump| dump.call(py, (metadata,), None))
+//             .and_then(|string| string.extract::<String>(py))
+//             .and_then(|string|
+//                 serde_yaml::from_str::<serde_yaml::Value>(&string)
+//                     .or_else(|err|
+//                         Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err)))))?;
+
+//         let context = context.context(py).clone();
+
+//         spor::anchor::Anchor::new(file_path, context, metadata, encoding)
+//             .or_else(|err| {
+//                 Err(PyErr::new::<cpython::exc::OSError, _>(py, format!("{}", err)))
+//             })
+//             .and_then(|anchor| {
+//                 Anchor::create_instance(py, anchor)
+//             })
+//     }
+
+//     def file_path(&self) -> PyResult<String> {
+//         let p = self.anchor(py).file_path();
+//         p.to_str()
+//             .ok_or(PyErr::new::<cpython::exc::ValueError, _>(py,
+//                 "Unable to convert path to string"))
+//             .map(|s| s.to_owned())
+//     }
+
+//     def encoding(&self) -> PyResult<String> {
+//         Ok(self.anchor(py).encoding().clone())
+//     }
+
+//     def context(&self) -> PyResult<Context> {
+//         Context::create_instance(py, self.anchor(py).context().clone())
+//     }
+
+//     def metadata(&self) -> PyResult<PyDict> {
+//         let metadata = self.anchor(py).metadata();
+
+//         let metadata_string = serde_yaml::to_string(metadata)
+//             .or_else(|err|
+//                 Err(PyErr::new::<cpython::exc::ValueError, _>(py, format!("{}", err))))?;
+
+//         PyModule::import(py, "yaml")
+//             .and_then(|yaml| yaml.get(py, "safe_load"))
+//             .and_then(|load| load.call(py, (metadata_string,), None))
+//             .and_then(|dict|
+//                 dict.cast_into::<PyDict>(py)
+//                     .or_else(|err| Err(PyErr::from(err))))
+//     }
+
+//     data anchor: spor::anchor::Anchor;
+// });
+
+// pub fn init_module(py: Python) -> PyResult<PyModule> {
+//     let m = PyModule::new(py, "anchor")?;
+//     m.add(py, "__doc__", "Anchor implementation")?;
+//     m.add_class::<Anchor>(py)?;
+//     m.add_class::<Context>(py)?;
+//     Ok(m)
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
-use cpython::{py_fn, py_module_initializer, PyResult, Python};
+use pyo3::prelude::*;
+use pyo3::{wrap_pymodule, wrap_pyfunction};
 use spor::alignment::align::Aligner;
 use spor::alignment::smith_waterman::{SimpleScorer, SmithWaterman};
 
 pub mod anchor;
-mod fs_repository;
+// mod fs_repository;
 
-fn align(_: Python, a: &str, b: &str) -> PyResult<f32> {
+#[pyfunction]
+fn align(a: &str, b: &str) -> PyResult<f32> {
     let scorer = SimpleScorer::default();
     let aligner = SmithWaterman::new(scorer);
     let (score, _) = aligner.align(a, b);
     Ok(score)
 }
 
-py_module_initializer!(spor, |py, m| {
-    m.add(py, "__doc__", "Anchored metadata.")?;
-    // TODO: Move align into module to match rust module structure.
-    m.add(py, "align", py_fn!(py, align(a: &str, b: &str)))?;
-    m.add(py, "anchor", anchor::init_module(py)?)?;
-    m.add(py, "fs_repository", fs_repository::init_module(py)?)?;
+#[pymodule]
+fn anchor(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<crate::anchor::PyContext>()?;
     Ok(())
-});
+}
+
+#[pymodule]
+/// A Python module implemented in Rust.
+fn spor(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(align))?;
+
+    m.add_wrapped(wrap_pymodule!(anchor))?;
+    Ok(())
+}
